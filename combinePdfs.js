8cce0d30cb888f23e594b5fcbddd233ac24d5883
mLuby <mLuby@users.noreply.github.com>
2018-06-25 13:16:10 -0500

    fixes single path bug and adds 100% coverage requirement

--- combinePdfs.js
+++ combinePdfs.js
@@ -1,13 +1,20 @@
-const {createWriteStream} = require("fs")
+const fs = require("fs")
+const {promisify} = require("util")
 const pdfmerger = require("pdfmerger")
 const streamToPromise = require("stream-to-promise")
 
 module.exports = combinePdfs
 
-async function combinePdfs (pdfPaths, combinedPdfPath) {
-  const pdfStream = pdfmerger(pdfPaths)
-  const writeStream = createWriteStream(combinedPdfPath)
+async function combinePdfs (paths, resultPath) {
+  if (!Array.isArray(paths) || paths.length === 0) {
+    throw new Error("Must have at least one path in array")
+  } else if (paths.length === 1) {
+    await promisify(fs.copyFile)(paths[0], resultPath)
+    return resultPath
+  }
+  const pdfStream = pdfmerger(paths)
+  const writeStream = fs.createWriteStream(resultPath)
   pdfStream.pipe(writeStream)
   await streamToPromise(pdfStream)
-  return combinedPdfPath
+  return resultPath
 }
--- imagesToPdf.js
+++ imagesToPdf.js
@@ -1,24 +1,20 @@
 const fs = require("fs")
 const {promisify} = require("util")
 const PdfPrinter = require("pdfmake")
-const Roboto64 = require("pdfmake/build/vfs_fonts.js").pdfMake.vfs // needed to get PdfPrinter to work
 const sizeOf = promisify(require("image-size"))
-const pdfmerger = require("pdfmerger")
+const combinePdfs = require("./combinePdfs.js")
 const streamToPromise = require("stream-to-promise")
 
 module.exports = imagesToPdf
 
-async function imagesToPdf (imagePaths, pdfPath) {
-  const pagePaths = await Promise.all(imagePaths.map(createOnePagePdfFromImage))
-  const pdfStream = pdfmerger(pagePaths)
-  const writeStream = fs.createWriteStream(pdfPath)
-  pdfStream.pipe(writeStream)
-  await streamToPromise(pdfStream)
+async function imagesToPdf (paths, resultPath) {
+  const pagePaths = await Promise.all(paths.map(createOnePagePdfFromImage))
+  await combinePdfs(pagePaths, resultPath)
   await Promise.all(pagePaths.map(async pagePath => await promisify(fs.unlink)(pagePath)))
-  return pdfPath
+  return resultPath
 }
 
-async function createOnePagePdfFromImage (imagePath) {
+async function createOnePagePdfFromImage (imagePath, page) {
   const pageSize = await sizeOf(imagePath)
   const docDefinition = {
     content: {image: imagePath},
@@ -26,21 +22,10 @@ async function createOnePagePdfFromImage (imagePath) {
     pageOrientation: pageSize.width > pageSize.height ? "landscape" : "portrait",
     pageSize,
   }
-  const doc = new PdfPrinter(fonts()).createPdfKitDocument(docDefinition)
-  const pagePath = `${imagePath}.pdf`
+  const doc = new PdfPrinter().createPdfKitDocument(docDefinition)
+  const pagePath = `${page}.pdf`
   doc.pipe(fs.createWriteStream(pagePath))
   doc.end()
   await streamToPromise(doc)
   return pagePath
-}
-
-function fonts () { // needed to get PdfPrinter to work
-  return {
-    Roboto: {
-      bold: new Buffer(Roboto64["Roboto-Medium.ttf"], "base64"),
-      bolditalics: new Buffer(Roboto64["Roboto-MediumItalic.ttf"], "base64"),
-      italics: new Buffer(Roboto64["Roboto-Italic.ttf"], "base64"),
-      normal: new Buffer(Roboto64["Roboto-Regular.ttf"], "base64"),
-    },
-  }
 }
